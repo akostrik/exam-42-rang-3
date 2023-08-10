d4e6fc8ccec818bf7af4737ad1a65019125b58cc
akostrik <internet124816@gmail.com>
2023-08-11 00:54:30 +0200

    -

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-`ft_printf` that mimics the real printf but it manages only the following conversions:
+Write a function named ft_printf that will mimic the real printf but it will manage only the following conversions:
 ```
 s (string)
 d (decimal) 
@@ -8,4 +8,21 @@ x (lowercase hexademical)
 ## Allowed Functions
 ```
 malloc, free, write, va_start, va_arg, va_copy and va_end
+```
+
+##Prototype
+```
+int ft_printf(const char *, ... )
+```
+
+##Output Examples
+```
+call: ft_printf("%s\n", "toto")
+out: toto$
+
+call: ft_printf("Magic %s is %d", "number", 42)
+out: Magic number is 42%
+
+call: ft_printf("Hexadecimal for %d is %x\n", 42, 42)
+out: Hexadecimal for 42 is 2a$
 ```
--- ft_printf.c
+++ ft_printf.c
@@ -3,15 +3,33 @@
 #include <stdio.h>
 #include <limits.h>
 
-int	put_digit(long n, int base)
+int	put_number(long n, int base)
 {
 	char	*hex = "0123456789abcdef";
+	int len = 0;
+	int len2 = 0;
 
 	if (n < 0)
-		return (write(1, "-", 1) + put_digit(-n, base));
+	{
+		if (write(1, "-", 1) < 0)
+			return (-1);
+		len = put_number(-n, base);
+		if (len < 0)
+			return (-1);
+		return (1 + len);
+	}
 	if (n < base)
-		return (write(1, &hex[n], 1));
-	return (put_digit(n / base, base) + write(1, &hex[n % base], 1));
+	{
+		if (write(1, &hex[n], 1) < 0)
+			return (-1);
+		return (1);
+	}
+	len = put_number(n / base, base);
+	if (len < 0)
+		return (-1);
+	if (write(1, &hex[n % base], 1) < 0)
+		return (-1);
+	return (len + 1);
 }
 
 int	put_str(char *str)
@@ -21,26 +39,42 @@ int	put_str(char *str)
 	if (!str)
 		str = "(null)";
 	while (*str)
-		len += write(1, str++, 1);
+	{
+		if (write(1, str++, 1) < 0)
+			return (-1);
+		len ++;
+	}
 	return (len);
 }
 
 int	ft_printf(const char *f, ...)
 {
 	int len = 0;
+	int len0 = 0;
 	va_list	args;
 
+	if (f == NULL)
+		return (-1);
 	va_start(args, f);
 	while (*f)
 	{
 		if (*f == '%' && *(f + 1) == 'd')
-			len += put_digit((long)va_arg(args, int), 10);
+			len0 = put_number((long)va_arg(args, int), 10);
 		else if (*f == '%' && *(f + 1) == 'x')
-			len += put_digit((long)va_arg(args, unsigned int), 16);
+			len0 = put_number((long)va_arg(args, unsigned int), 16);
 		else if (*f == '%' && *(f + 1) == 's')
-			len += put_str(va_arg(args, char *));
+			len0 = put_str(va_arg(args, char *));
+		//else if (*f == '%' && *(f + 1) != 'd' && *(f + 1) != 'x' && *(f + 1) != 's')
+		//{
+		//}
 		else
-			len += write(1, f, 1);
+			len0 = write(1, f, 1);
+		if (len0 < 0)
+		{
+			va_end(args);
+			return (-1);
+		}
+		len += len0;
 		if (*f == '%' && (*(f + 1) == 'd' || *(f + 1) == 'x' || *(f + 1) == 's'))
 			f++;
 		f++;
@@ -51,15 +85,42 @@ int	ft_printf(const char *f, ...)
 
 int main()
 {
-	int n2 =    printf("%d %d %d %d %d %d %d %d %d", -0, 1, 99, 100, -42, INT_MAX, INT_MIN, INT_MAX+1, INT_MIN-1); printf(" (return %d)\n", n2);
-	int n1 = ft_printf("%d %d %d %d %d %d %d %d %d", -0, 1, 99, 100, -42, INT_MAX, INT_MIN, INT_MAX+1, INT_MIN-1); printf(" (return %d)\n", n1);
-	printf("\n");
-	int n3 =    printf("%x %x %x %x %x %x %x %x %x", -0, 1, 255, 256, -42, INT_MAX, INT_MIN, INT_MAX+1, INT_MIN-1); printf(" (return %d)\n", n3);
-	int n4 = ft_printf("%x %x %x %x %x %x %x %x %x", -0, 1, 255, 256, -42, INT_MAX, INT_MIN, INT_MAX+1, INT_MIN-1); printf(" (return %d)\n", n4);
-	printf("\n");
-	int n5 =    printf("%x %x %x %x %x", 42, LONG_MAX, LONG_MAX + 1, LONG_MIN - 1, LONG_MIN); printf(" (return %d)\n", n5);
-	int n6 = ft_printf("%x %x %x %x %x", 42, LONG_MAX, LONG_MAX + 1, LONG_MIN - 1, LONG_MIN); printf(" (return %d)\n", n6);
-	printf("\n");
-	int n7 =    printf("%s %s %s", NULL, "", "toto"); printf(" (return %d)\n", n7);
-	int n8 = ft_printf("%s %s %s", NULL, "", "toto"); printf(" (return %d)\n", n8);
+	int n2  =    printf("%d %d %d %d %d %d %d %d", 0, 1, -1, 99, 100, 101, -42, 42+2); printf(" (return %d)\n", n2);
+	int n1  = ft_printf("%d %d %d %d %d %d %d %d", 0, 1, -1, 99, 100, 101, -42, 42+2); printf(" (return %d)\n\n", n1);
+
+	int n9  =    printf("%d %d %d %d %d %d %d %d", INT_MAX, INT_MIN, INT_MAX+1, INT_MIN-1, LONG_MAX, LONG_MAX + 1, LONG_MIN - 1, LONG_MIN); printf(" (return %d)\n", n9);
+	int n10 = ft_printf("%d %d %d %d %d %d %d %d", INT_MAX, INT_MIN, INT_MAX+1, INT_MIN-1, LONG_MAX, LONG_MAX + 1, LONG_MIN - 1, LONG_MIN); printf(" (return %d)\n\n", n10);
+
+	int n5  =    printf("%x %x %x %x %x %x %x %x", 0, 1, -1, 255, 256, 257, -42, 42+2); printf(" (return %d)\n", n5);
+	int n6  = ft_printf("%x %x %x %x %x %x %x %x", 0, 1, -1, 255, 256, 257, -42, 42+2); printf(" (return %d)\n\n", n6);
+
+	int n3  =    printf("%x %x %x %x %x %x %x %x", INT_MAX, INT_MIN, INT_MAX+1, INT_MIN-1, LONG_MAX, LONG_MAX + 1, LONG_MIN - 1, LONG_MIN); printf(" (return %d)\n", n3);
+	int n4  = ft_printf("%x %x %x %x %x %x %x %x", INT_MAX, INT_MIN, INT_MAX+1, INT_MIN-1, LONG_MAX, LONG_MAX + 1, LONG_MIN - 1, LONG_MIN); printf(" (return %d)\n\n", n4);
+
+	int n7  =    printf("%s %s %s %s %s %s %s %s", NULL, "", "\0", "toto", " ", 0, "\n", "\t"); printf(" (return %d)\n", n7);
+	int n8  = ft_printf("%s %s %s %s %s %s %s %s", NULL, "", "\0", "toto", " ", 0, "\n", "\t"); printf(" (return %d)\n\n", n8);
+
+	int n13  =    printf(NULL); printf(" (return %d)\n", n13);
+	int n14  = ft_printf(NULL); printf(" (return %d)\n\n", n14);
+
+	int n15  =    printf(NULL, 42); printf(" (return %d)\n", n15);
+	int n16  = ft_printf(NULL, 42); printf(" (return %d)\n\n", n16);
+
+	int n17  =    printf(""); printf(" (return %d)\n", n17);
+	int n18  = ft_printf(""); printf(" (return %d)\n\n", n18);
+
+	int n19  =    printf("", 42); printf(" (return %d)\n", n19);
+	int n20  = ft_printf("", 42); printf(" (return %d)\n\n", n20);
+
+	int n21  =    printf("%d %x %s"); printf(" (return %d)\n", n21);
+	int n22  = ft_printf("%d %x %s"); printf(" (return %d)\n\n", n22);
+
+	int n23  =    printf("%d %d %d", NULL, "toto", 99999999999999999999); printf(" (return %d)\n", n23);
+	int n24  = ft_printf("%d %d %d", NULL, "toto", 99999999999999999999); printf(" (return %d)\n\n", n24);
+
+	int n25  =    printf("%x %x %x", NULL, "toto", 99999999999999999999); printf(" (return %d)\n", n25);
+	int n26  = ft_printf("%x %x %x", NULL, "toto", 99999999999999999999); printf(" (return %d)\n\n", n26);
+
+	int n27  =    printf("%r", 42); printf(" (return %d)\n", n27);
+	int n28  = ft_printf("%r", 42); printf(" (return %d)\n\n", n28);
 }
